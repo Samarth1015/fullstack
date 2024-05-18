a74f4d353cf2e875c55da296aa0cc5d67d006852
samarth1014 <samarth.acharya2005@gmail.com>
2024-05-19 01:58:39 +0530

    day 18

---
+++ front_end/day18/script.js
@@ -0,0 +1,33 @@
+const thread = {
+  username: "samarth",
+  content: "nature photo",
+  likes: 23,
+  repost: 2,
+  tags: 3,
+};
+console.log(thread);
+
+let num = Math.random();
+num = num * 100;
+num = Math.floor(num) + 1;
+console.log(`Number between 1 to 100 is ${num}`);
+// dice roll
+let num1 = Math.random();
+num1 = num1 * 6;
+num1 = Math.floor(num1) + 1;
+console.log(`Number between 1 to 6 is ${num1}`);
+
+const car = {
+  name: "verna",
+  model: 300,
+  color: "black",
+};
+console.log(car.name);
+const person = {
+  name: "samarth",
+  age: "18",
+  city: "indore",
+};
+person.city = "New york";
+person.country = "USA";
+console.log(person);
