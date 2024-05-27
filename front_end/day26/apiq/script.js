612d7da835e8780c9561d0a5c4839179948916a8
samarth <samarth.acharya2005@gmail.com>
2024-05-27 22:55:15 +0530

    s

--- front_end/day26/apiq/script.js
+++ front_end/day26/apiq/script.js
@@ -28,7 +28,8 @@ let url1 = "https://dog.ceo/api/breeds/image/random";
 // getFAct();
 
 let btn = document.querySelector("button");
-btn.addEventListener("click", setimage);git
+btn.addEventListener("click", setimage);
+git;
 
 async function setimage() {
   let imageurl = await axios.get(url1);
