ae2df4dd2f1e1725bd6f387fc7cd35efdfbde74a
samarth <samarth.acharya2005@gmail.com>
2024-06-03 01:32:10 +0530

    ejs

--- backend/ejdir/index.js
+++ backend/ejdir/index.js
@@ -2,8 +2,9 @@ const port = 8080;
 const express = require("express");
 const path = require("path");
 const app = express();
-app.set("view engiene", "ejs");
-app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.listen(port, () => {
   console.log("working on 8080");
@@ -11,4 +12,14 @@ app.listen(port, () => {
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
+});
+app.get("/rolldice", (req, res) => {
+  const dicevalue = Math.floor(Math.random() * 6) + 1;
+  res.render("rolldice.ejs", { dicevalue });
+});
+app.get("/ig/:username", (req, res) => {
+  let { username } = req.params;
+  let instadata = require("./data.json");
+  let userspecificdata = instadata[username];
+  res.render("insta.ejs", { userspecificdata });
 });
